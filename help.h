d809a174ad60c204f1fa5f8e8eb4d030272fb7cb
Randolph Chung <tausq@debian.org>
1999-12-05 19:03:11 +0000

    Fixed some typos

--- help.h
+++ help.h
@@ -1,14 +1,14 @@
-/* $Id: help.h,v 1.1 1999/11/21 22:01:04 tausq Exp $ */
+/* $Id: help.h,v 1.2 1999/12/05 19:03:11 tausq Exp $ */
 #ifndef _HELP_H
 #define _HELP_H
 
 #define HELPTXT _("Task packages are \"metapackages\" that allow you to quickly install a selection of " \
-		  "packages that performs a given task.\n\nThe main chooser list shows a list of " \
+		  "packages that performs a given task.\r\rThe main chooser list shows a list of " \
 		  "tasks that you can choose to install. The arrow keys moves the cursor. Pressing ENTER " \
 		  "or the SPACEBAR toggles the selection of the package at the cursor. You can also press " \
 		  "A to select all packages, or N to deselect all packages. Pressing Q will exit this " \
-		  "program and begin installation of your selected tasks.\n\n\nThank you for using Debian." \
-		  "\n\nPress enter to return to the task selection screen" \
+		  "program and begin installation of your selected tasks.\r\rThank you for using Debian." \
+		  "\r\rPress enter to return to the task selection screen" \
 		 )
 
 #endif
